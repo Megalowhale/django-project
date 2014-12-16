9e7611775c89b4aae2d156693d50eb6ee572aed8
Megalowhale <Megalowhale@gmail.com>
2014-12-15 23:35:03 -0500

    Changed static HTML to django templates.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
